f5cc4c9692e858b8f78ec45509332819180694ff
Fernando Pinto Lino <pintolinofernando@gmail.com>
2024-11-26 07:46:45 -0400

    CRUD CATEGORIA

--- resources/js/My_Components/Menu.vue
+++ resources/js/My_Components/Menu.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import {Link} from '@inertiajs/vue3';
 </script>
 
 <template>
@@ -31,7 +31,7 @@
                             </form>
                         </li>
                         <li>
-                            <a class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
+                            <Link :href="route('dashboard')" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
                                 <svg
                                     class="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                                     fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -39,15 +39,15 @@
                                     <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
                                 </svg>
                                 <span class="ml-3" sidebar-toggle-item>Dashboard</span>
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a :href="route('categorias.index')" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
+                            <Link :href="route('categorias.index')" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
                                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-width="2" d="M3 11h18M3 15h18m-9-4v8m-8 0h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
                                 </svg>
                                 <span class="ml-3" sidebar-toggle-item>Categorias</span>
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
--- resources/js/Pages/Categorias/Editar.vue
+++ resources/js/Pages/Categorias/Editar.vue
@@ -36,8 +36,8 @@ const fecha = (fechaData) => {
         <section class="bg-white dark:bg-gray-900">
             <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
                 <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Editar categoria</h2>
-                <p>Fecha Creado: {{ Utils.fecha(props.model.created_at) }}</p>
-                <p>Fecha Actualizado: {{ Utils.fecha(props.model.updated_at) }}</p>
+                <p class="mb-4 text-gray-900 dark:text-white">Fecha Creado: {{ Utils.fecha(props.model.created_at) }}</p>
+                <p class="mb-4 text-gray-900 dark:text-white">Fecha Actualizado: {{ Utils.fecha(props.model.updated_at) }}</p>
                 <form @submit.prevent="submit">
                     <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                         <div class="sm:col-span-2">
@@ -63,6 +63,14 @@ const fecha = (fechaData) => {
                     </div>
                     <button type="submit"
                             class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+                            <path fill-rule="evenodd"
+                                  d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                                  clip-rule="evenodd"></path>
+                        </svg>
                         Editar Categoria
                     </button>
                 </form>
